5da691ec0a66c858e55c4fd5a53a9f373a7230a0
Hellyna NG <hellyna@groventure.com>
2015-10-13 17:22:30 +0800

    Now sensu-api can set initial user and password, with other changes
    + Switched config generation to templates.
    + Entrypoint bash set to not use end of flags marker "--".

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,8 @@ FROM debian:jessie
 MAINTAINER Hellyna NG <hellyna@groventure.com>
 
 COPY scripts/* /docker-scripts/
+COPY templates/* /docker-templates/
 RUN /bin/bash /docker-scripts/build.sh
 
 VOLUME ["/etc/sensu"]
-ENTRYPOINT ["/bin/bash", "/docker-scripts/entrypoint.sh", "--"]
+ENTRYPOINT ["/bin/bash", "/docker-scripts/entrypoint.sh"]
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -25,41 +25,59 @@ if [[ -z "$RABBITMQ_ENV_RABBITMQ_DEFAULT_PASS" ]]; then
   exit 1
 fi
 
-localconf_path='/etc/sensu/config.json'
+case "$1" in
+  api)
+    template='/docker-templates/api-config.tmpl.json'
+    if [[ -z "$API_USER" ]]; then
+      echo '$API_USER not defined. Aborting...' >&2
+      exit 1
+    fi
 
-if [[ "$1" == 'client' ]]; then
-  cat > "$localconf_path" <<EOF
-{
-  "rabbitmq": {
-    "host": "$RABBITMQ_PORT_5672_TCP_ADDR",
-    "vhost": "$RABBITMQ_ENV_RABBITMQ_DEFAULT_VHOST",
-    "user": "$RABBITMQ_ENV_RABBITMQ_DEFAULT_USER",
-    "password": "$RABBITMQ_ENV_RABBITMQ_DEFAULT_PASS"
-  }
+    if [[ -z "$API_PASSWORD" ]]; then
+      echo '$API_PASSWORD not defined. Aborting...' >&2
+      exit 1
+    fi
+    ;;
+  client)
+    template='/docker-templates/client-config.tmpl.json'
+    ;;
+  server)
+    template='/docker-templates/server-config.tmpl.json'
+    ;;
+esac
+config='/etc/sensu/config.json'
+
+install -Zm 600 "$template" "$config"
+
+function escape_sed {
+  echo "$1" | sed -r 's/\//\\\//g'
 }
-EOF
-else
-  if [[ -z "$REDIS_PORT_6379_TCP_ADDR" ]]; then
-    echo '$REDIS_PORT_6379_TCP_ADDR not defined. Aborting...' >&2
-    exit 1
-  fi
-  cat > "$localconf_path" <<EOF
-{
-  "rabbitmq": {
-    "host": "$RABBITMQ_PORT_5672_TCP_ADDR",
-    "vhost": "$RABBITMQ_ENV_RABBITMQ_DEFAULT_VHOST",
-    "user": "$RABBITMQ_ENV_RABBITMQ_DEFAULT_USER",
-    "password": "$RABBITMQ_ENV_RABBITMQ_DEFAULT_PASS"
-  },
-  "redis": {
-    "host": "$REDIS_PORT_6379_TCP_ADDR"
-  }
-}
-EOF
+
+sed -i.tmp 's/{{\s*RABBITMQ_PORT_5672_TCP_ADDR\s*}}/'"$(escape_sed "$RABBITMQ_PORT_5672_TCP_ADDR")"'/g' \
+  "$config"
+sed -i.tmp 's/{{\s*RABBITMQ_ENV_RABBITMQ_DEFAULT_VHOST\s*}}/'"$(escape_sed "$RABBITMQ_ENV_RABBITMQ_DEFAULT_VHOST")"'/g' \
+  "$config"
+sed -i.tmp 's/{{\s*RABBITMQ_ENV_RABBITMQ_DEFAULT_USER\s*}}/'"$(escape_sed "$RABBITMQ_ENV_RABBITMQ_DEFAULT_USER")"'/g' \
+  "$config"
+sed -i.tmp 's/{{\s*RABBITMQ_ENV_RABBITMQ_DEFAULT_PASS\s*}}/'"$(escape_sed "$RABBITMQ_ENV_RABBITMQ_DEFAULT_PASS")"'/g' \
+  "$config"
+
+if [[ "$1" != 'client' ]]; then
+  sed -i.tmp 's/{{\s*REDIS_PORT_6379_TCP_ADDR\s*}}/'"$(escape_sed "$REDIS_PORT_6379_TCP_ADDR")"'/g' \
+    "$config"
 fi
 
+if [[ "$1" == 'api' ]]; then
+  sed -i.tmp 's/{{\s*API_USER\s*}}/'"$(escape_sed "$API_USER")"'/g' \
+    "$config"
+  sed -i.tmp 's/{{\s*API_PASSWORD\s*}}/'"$(escape_sed "$API_PASSWORD")"'/g' \
+    "$config"
+fi
+
+rm -vf "${config}.tmp"
+
 exec "/opt/sensu/bin/sensu-$1" \
-  -c "$localconf_path" \
+  -c "$config" \
   -d '/etc/sensu/conf.d' \
   -e '/etc/sensu/extensions' \
   "$@"
